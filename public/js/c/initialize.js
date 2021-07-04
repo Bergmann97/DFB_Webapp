ba7e17557272401e7ef6018cae51d4bd37a4f176
minaleebtu <57717218+minaleebtu@users.noreply.github.com>
2021-07-04 19:14:25 +0200

    added implementation of authentication

--- public/js/c/initialize.js
+++ public/js/c/initialize.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview  Defining the main namespace ("public library") and its MVC subnamespaces
- * @authors Gerd Wagner & Juan-Francisco Reyes
+ * @authors Gerd Wagner & Juan-Francisco Reyes (modified by Mina Lee)
  */
 'use strict';
 // main namespace pl = "public library"
